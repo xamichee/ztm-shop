c1eefc8d6483c333212814c8614a6442e10d7c37
Konstantin Golovan <mindrot@yandex.ru>
2021-08-30 16:24:04 +0300

    added sign-up functions

--- src/SignIn/SignIn.jsx
+++ src/SignIn/SignIn.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import FormInput from "../FormInput/FormInput";
 import Button from "../Button/Button";
 
-import {signInWithGoogle} from '../Firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../Firebase/firebase.utils';
 
 import './SignIn.scss'
 
@@ -10,26 +10,30 @@ function SignIn() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const [ emailInput, setEmailInput] = useState('');
-  const [ passwordInput, setPasswordInput] = useState('');
 
   const handleChange = ({target}) => {
     const { value, name } = target;
     // eslint-disable-next-line default-case
     switch (name) {
       case 'email':
-        setEmailInput(value);
+        setEmail(value);
         return
       case 'password':
-        setPasswordInput(value);
+        setPassword(value);
         return
     }
   }
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    setEmail(emailInput);
-    setPassword(passwordInput);
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      setEmail('');
+      setPassword('');
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   return (
@@ -38,8 +42,8 @@ function SignIn() {
       <span>Sign in with your email and password</span>
 
       <form onSubmit={handleSubmit}>
-        <FormInput name="email" value={emailInput} type="email" required handleChange={handleChange} label="Email" />
-        <FormInput name="password" value={passwordInput} type="password" required handleChange={handleChange} label="Password" />
+        <FormInput name="email" value={email} type="email" required handleChange={handleChange} label="Email" />
+        <FormInput name="password" value={password} type="password" required handleChange={handleChange} label="Password" />
         <div className="buttons">
           <Button type="submit" children={"Submit"} />
           <Button onClick={signInWithGoogle} children={"Sign in with Google"} isGoogleSignIn />
