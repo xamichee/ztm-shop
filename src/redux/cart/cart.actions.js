8e1c93929ee013d196419c95966b9bba763164c0
Konstantin Golovan <mindrot@yandex.ru>
2021-09-02 10:00:44 +0300

    added remove and add item functionality

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -7,4 +7,15 @@ export const toggleCartHidden = () => ({
 export const addItem = item => ({
   type: cartActionTypes.ADD_ITEM,
   payload: item,
-})
+});
+
+export const removeItem = item => ({
+  type: cartActionTypes.REMOVE_ITEM,
+  payload: item,
+});
+
+export const clearItem = item => ({
+  type: cartActionTypes.CLEAR_ITEM_FROM_CART,
+  payload: item,
+});
+
